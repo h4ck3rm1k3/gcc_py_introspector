cb5d030b0e16f4fbd045a830f8f4d09825d0c822
jamesmikedupont <jamesmikedupont@gmail.com>
2016-03-30 03:17:45 +0000

    cleanup

--- data/body3.py
+++ data/body3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import pprint
+#import pprint
 
 def result_decl(**kwargs):
     return "return"
@@ -9,111 +9,171 @@ def var_decl(**kwargs):
     return kwargs['name']
 
 def identifier_node(**kwargs):
-        return kwargs['string']
+    return kwargs['string']
+
+######################
 
 def call_expr(**kwargs):
-    #pprint.pprint({"call" : kwargs})
-    return kwargs
+    return {"call" : kwargs}
+
+def decl_expr(**kwargs):
+#    pprint.pprint({"decl_expr" : kwargs})
+    #return {"decl_expr" : kwargs}
+    return None
+
+def expr(**kwargs):
+#    pprint.pprint({"expr" : kwargs})
+    return {"expr" : kwargs}
+
+def cond_expr(**kwargs):
+#    pprint.pprint({"cond_expr" : kwargs})
+    return {"cond_expr" : kwargs}
+
+def ne_expr(**kwargs):
+#    pprint.pprint({"ne_expr" : kwargs})
+    return {"ne_expr" : kwargs}
 
 def addr_expr(**kwargs):
     #pprint.pprint({"addr_expr" : kwargs})
     return kwargs['type']
 
-def function_decl(**kwargs):
-    #pprint.pprint({"function_decl" : kwargs})
-    return "Func(" + kwargs['name'] + ")"
+import yaml
+
+
 
 def modify_expr(**kwargs):
-    pprint.pprint({"Modify":kwargs})
-    return kwargs
-
-def pointer_type(**kwargs):
-                pass
-
-def component_ref(**kwargs):
-                pass
-def OP0(**kwargs):
-                pass
-def OP1(**kwargs):
-                pass
-def vars(**kwargs):
-                pass
+#    pprint.pprint({"Modify":kwargs})
+    return {"Modify":kwargs}
 
 def truth_andif_expr(**kwargs):
-                pass
+#    pprint.pprint({"truth_andif_expr" : kwargs})
+    return {"truth_andif_expr" : kwargs}
+
+def nop_expr(**kwargs):
+#    pprint.pprint({"nop_expr" : kwargs})
+    #return {"nop" : kwargs}
+    return kwargs['OP0']
+
+def bind_expr(**kwargs):
+#    pprint.pprint({"bind_expr" : kwargs})
+    return {"bind" : kwargs}
+
+def return_expr(**kwargs):
+#    pprint.pprint({"return_expr" : kwargs})
+    return {"ret" : kwargs}
+
+def eq_expr(**kwargs):
+#    pprint.pprint({"eq_expr" : kwargs})
+    return {"eq" : kwargs}
+
+            
+def pointer_type(**kwargs):
+#    pprint.pprint({"pointer_type" : kwargs})
+    return {"ptr" : kwargs}
+
+def component_ref(**kwargs):
+#    pprint.pprint({"component_ref" : kwargs})
+    return {"comp_ref" : kwargs}
+
+def OP0(**kwargs):
+    return kwargs
+
+def OP1(**kwargs):
+    return kwargs
+
+def vars(**kwargs):
+    #pprint.pprint({"vars" : kwargs})
+    return {"vars" : kwargs}
+
 def type(**kwargs):
-                pass
+    return {'type':kwargs}
+
 def integer_cst(**kwargs):
-         return kwargs['low']
+    return kwargs['low']
+
 def size(**kwargs):
-                pass
+    return kwargs
+
 def note(**kwargs):
-                pass
-       
+    return kwargs       
 
 def low(**kwargs):
-                pass
+    #pprint.pprint({"low" : kwargs})
+    return {"low" : kwargs}
+
 def body(**kwargs):
-                pass
+    #pprint.pprint({"body" : kwargs})
+    return {"body" : kwargs}
+
 def used(**kwargs):
-                pass
+    return kwargs
+
 def string(**kwargs):
-                pass
-def return_expr(**kwargs):
-                pass
+    #pprint.pprint({"string" : kwargs})
+    return {"string" : kwargs}
+
 def string_cst(**kwargs):
-                pass
+    #pprint.pprint({"string_cst" : kwargs})
+    return {"string_cst" : kwargs}
     
-def nop_expr(**kwargs):
-                pass
 def srcp(**kwargs):
-                pass
-def eq_expr(**kwargs):
-                pass
+    return kwargs
+
 def link(**kwargs):
-                pass
-def bind_expr(**kwargs):
-                pass
+    return kwargs
+
 def fn(**kwargs):
-                pass
+    #pprint.pprint({"fn" : kwargs})
+    return {"fn" : kwargs}
+
 def name(**kwargs):
-                pass
-def decl_expr(**kwargs):
-                pass
-def expr(**kwargs):
-                pass
-def cond_expr(**kwargs):
-                pass
-def ne_expr(**kwargs):
-                pass
+    #pprint.pprint({"name" : kwargs})
+    return {"name" : kwargs}
     
 def bpos(**kwargs):
-                pass
+    return kwargs
+
 def algn(**kwargs):
-                pass
+    return kwargs
+
 def field_decl(**kwargs):
-                pass
+    #pprint.pprint({"field_decl" : kwargs})
+    return {"fld" : kwargs}
+
 def statement_list(**kwargs):
-                pass
+    #pprint.pprint({"statement_list" : kwargs})
+    r = []
+    for x in sorted(kwargs.keys()):
+        v = kwargs[x]
+        if v:
+            r.append(kwargs[x])
+    #return {"stmts" : kwargs}
+    return r
+
 def E2(**kwargs):
-                pass
+    return kwargs
 def E8(**kwargs):
-                pass
+    return kwargs
 def E5(**kwargs):
-                pass
+    return kwargs
 def E4(**kwargs):
-                pass
+    return kwargs
 def E7(**kwargs):
-                pass
+    return kwargs
 def E6(**kwargs):
-                pass
+    return kwargs
 def E1(**kwargs):
-                pass
+    return kwargs
 def E0(**kwargs):
-                pass
+    pprint.pprint(kwargs)
+    return kwargs
 def E3(**kwargs):
-                pass
+    return kwargs
 
 def Unknown():
-    pass
+    return "Unknown"
 
+def function_decl(**kwargs):
+    print yaml.dump({"function": kwargs})
+    #pprint.pprint({"function_decl" : kwargs})
+    return "Func(" + kwargs['name'] + ")"
